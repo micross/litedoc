e95eb902473240a19f7716168bfd6cbd1b3f4a35
micross <466066211@qq.com>
2018-07-24 10:46:43 +0800

    update

--- models/token.go
+++ models/token.go
@@ -11,12 +11,12 @@ type Token struct {
 	Revoked   bool      `json:"revoked"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
-	ExpiresAt time.Time `json:"expires_at`
+	ExpiresAt time.Time `json:"expires_at"`
 }
 
 type RefreshToken struct {
 	Id        int       `json:"id"`
 	TokenId   int       `json:"token_id"`
 	Revoked   bool      `json:"revoked"`
-	ExpiresAt time.Time `json:"expires_at`
+	ExpiresAt time.Time `json:"expires_at"`
 }
